8ed9843db4216a3e3265b984f51458fd311876e5
Shivam Verma <shivamthegreat.sv@gmail.com>
2023-03-22 12:52:53 +0530

    feat: added support for lambda

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,9 @@ export const config = {
   port: process.env.PORT || 3000,
   redisHost: process.env.REDIS_HOST || 'localhost',
   redisPort: process.env.REDIS_PORT || 6379,
-  awsRegion: process.env.AWS_REGION || 'us-west-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  awsRegion: process.env.REGION_AWS || 'us-west-1',
+  accessKeyId: process.env.ACCESS_KEY_AWS,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY_AWS,
   consumerQueueName: process.env.CONSUMER_NAME, // platform listener queue for platform crons
   consumerQueueUrl: process.env.CONSUMER_URL,
   producerQueueName: process.env.PRODUCER_NAME, // any event to be sent out by service
